101e449a5968678bd25ca9129076f8c2089612d7
Macbook <macbook@MacBook-Pro.local>
2024-04-06 21:04:26 +0500

    OpenWeatherMap

---
+++ OpenWeatherMap.js
@@ -0,0 +1,8 @@
+let API = ' https://openweathermap.org/api'
+let description = ``
+
+translate:{
+ en: [`This free weather API provides current and forecast weather information for anywhere in the world`],
+ uz: [`Bu bepul ob-havo API dunyodagi har qanday joy uchun joriy va prognoz ob-havo ma'lumotlarini taqdim etadi`],
+ ru: [`Этот бесплатный API погоды предоставляет текущую и прогнозируемую информацию о погоде в любой точке мира.`]
+}
