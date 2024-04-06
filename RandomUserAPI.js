041fa67e87423a3f4844c82de87e709277abaf5e
Shahzodbek <136874725+Shakxzodbee@users.noreply.github.com>
2024-04-06 21:22:13 +0500

    Create RandomUserAPI.js

---
+++ RandomUserAPI.js
@@ -0,0 +1,8 @@
+let API = 'https://randomuser.me/'
+let description = ``
+
+translate:{
+ en: [`This free API generates random Lorem Ipsum text for use as placeholder text in design and development.`],
+ uz: [`Bu bepul API tasodifiy foydalanuvchi ma'lumotlarini, shuningdek nom, elektron pochta, manzil va profil rasmini generatsiya qiladi`],
+ ru: [`Этот бесплатный API генерирует случайный текст Lorem Ipsum для использования в качестве текста-заполнителя при проектировании и разработке.`]
+}
