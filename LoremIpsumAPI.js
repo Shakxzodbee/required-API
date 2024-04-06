77d878771d76a4e472ddff00c13ea777a4cbd90c
Shahzodbek <136874725+Shakxzodbee@users.noreply.github.com>
2024-04-06 21:12:37 +0500

    Create LoremIpsumAPI.js

---
+++ LoremIpsumAPI.js
@@ -0,0 +1,8 @@
+let API = 'https://loripsum.net/'
+let description = ``
+
+translate:{
+ en: [`This free API generates random Lorem Ipsum text for use as placeholder text in design and development.`],
+ uz: [`Bu bepul API tasarim va mahsulotda joy tutuvchi matn sifatida uzatish uchun yuklabiy Lorem Ipsum matni generatsiya qiladi.`],
+ ru: [`Этот бесплатный API генерирует случайный текст Lorem Ipsum для использования в качестве текста-заполнителя при проектировании и разработке.`]
+}
