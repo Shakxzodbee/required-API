9515112814291a099b952265afa3e9dc7c07c817
Shahzodbek <136874725+Shakxzodbee@users.noreply.github.com>
2024-04-07 22:50:11 +0500

    UnsplashAPI.js

---
+++ UnsplashAPI.js
@@ -0,0 +1,8 @@
+let API = 'https://unsplash.com/developers'
+let description = ``
+
+translate:{
+ en: [`This free API provides access to high-quality stock images`],
+ uz: [`Bu bepul API yuqori sifatli to'plam rasmlarga kirishni taqdim etadi`],
+ ru: [`Этот бесплатный API обеспечивает доступ к высококачественным стоковым изображениям.`]
+}
