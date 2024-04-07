6fc26af4e10a042255c73bb0bdb150321aa52314
Shahzodbek <136874725+Shakxzodbee@users.noreply.github.com>
2024-04-07 22:55:57 +0500

    Create PokeAPI.js

---
+++ PokeAPI.js
@@ -0,0 +1,8 @@
+let API = ' https://pokeapi.co//'
+let description = ``
+
+translate:{
+ en: [`This free API provides access to information about Pokemon characters, moves, and more`],
+ uz: [` Bu bepul API Pokemon personajlari, harakatlar va boshqalar to'g'risida ma'lumotlarga kirishni taqdim etadi`],
+ ru: [`Этот бесплатный API обеспечивает доступ к информации о персонажах, движениях и многом другом покемонов.`]
+}
