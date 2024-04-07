111e961fc1d6615c26e89bb656a18b7292401018
Shahzodbek <136874725+Shakxzodbee@users.noreply.github.com>
2024-04-07 22:54:01 +0500

    TheDogAPI.js

---
+++ TheDogAPI.js
@@ -0,0 +1,8 @@
+let API = 'https://thedogapi.com/'
+let description = ``
+
+translate:{
+ en: [`This free API allows you to use a powerful set - dog pictures, authenticity and more`],
+ uz: [`Bu bepul API kuchli to'plamdan - it rasmlari, haqiqiyati va boshqalaridan foydalanishga imkon beradi`],
+ ru: [`Этот бесплатный API позволяет вам использовать мощный набор — изображения собак, подлинность и многое другое.`]
+}
