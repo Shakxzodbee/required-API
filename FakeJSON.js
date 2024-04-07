342fed042230483eb47f8d8e8449c9da714f72d8
Shahzodbek <136874725+Shakxzodbee@users.noreply.github.com>
2024-04-07 22:52:31 +0500

    Create FakeJSON.js

---
+++ FakeJSON.js
@@ -0,0 +1,8 @@
+let API = 'https://fakejson.com/'
+let description = ``
+
+translate:{
+ en: [`This free API generates random JSON data for testing and prototyping purposes`],
+ uz: [`Bu bepul API testlash va prototip qilish maqsadida tasodifiy JSON ma'lumotlarini generatsiya qiladi`],
+ ru: [`Этот бесплатный API генерирует случайные данные JSON для целей тестирования и прототипирования.`]
+}
